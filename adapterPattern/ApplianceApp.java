6057052a32f38a2fe7c68249894a92f3ed41e4fc
CorderoEarl <earlcordero18@gmail.com>
2023-10-19 12:09:08 +0800

    Cordero, Earl

--- adapterPattern/ApplianceApp.java
+++ adapterPattern/ApplianceApp.java
@@ -13,6 +13,15 @@ public class ApplianceApp {
         Speaker speaker = new Speaker();
         RemoteControllerFunction speakerRc = new SpeakerAdapter(speaker);
 
+        Laptop laptop = new Laptop();
+        PowerOutlet laptopPo = new LaptopAdapter(laptop);
+
+        Refrigerator refrigerator = new Refrigerator();
+        PowerOutlet refrigeratorPo = new RefrigeratorAdapter(refrigerator);
+
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+        PowerOutlet smartphoneChargerPo = new SmartphoneAdapter(smartphoneCharger);
+
         //turn on the appliances
         System.out.println(tvRc.powerOn());
         System.out.println(airConRc.powerOn());
@@ -25,5 +34,11 @@ public class ApplianceApp {
         System.out.println(airConRc.pressPlusButton());
         System.out.println(speakerRc.pressPlusButton());
 
+        System.out.println();
+
+        //plug in to charge
+        System.out.println(laptopPo.plugIn());
+        System.out.println(refrigeratorPo.plugIn());
+        System.out.println(smartphoneChargerPo.plugIn());
     }
 }
---
+++ adapterPattern/SmartphoneCharger.java
@@ -0,0 +1,8 @@
+package adapterPattern;
+
+public class SmartphoneCharger {
+    public String chargePhone(){
+        return "Phone is now Charging!";
+    }
+
+}
